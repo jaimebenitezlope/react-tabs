7922214dd1b4477d89fcce87d2f4fc0ed9045df5
jaimebenitezlope <jaimebenitezlope@icloud.com>
2024-03-07 10:28:13 +0100

    10:28

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Tabs from './components/tabs/Tabs';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<h1>Núcleo de la aplicación</h1>
+			<Tabs />
 		</>
 	);
 };
---
+++ src/components/tabs/Tabs.jsx
@@ -0,0 +1,5 @@
+const Tabs = () => {
+	return <h1>Tabs</h1>;
+};
+
+export default Tabs;
